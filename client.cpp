4d44902c7590233d23aa01ed75c5b647e480acf3
Omor Faruk Mashuk <mofaruk@uwaterloo.ca>
2024-07-12 07:53:23 -0400

    updated input processing

--- client.cpp
+++ client.cpp
@@ -1,67 +1,59 @@
 #include <iostream>
-#include <cstring>
+#include <vector>
+#include <string>
+#include <cstdlib>
 #include <unistd.h>
 #include <arpa/inet.h>
-#include <sys/types.h>
 #include <sys/socket.h>
-#include <cstdlib>
 
-#define BUFFER_SIZE 1024
+// Convert hex string to binary data
+std::vector<char> hexToBinary(const std::string& hex) {
+    std::vector<char> binaryData;
+    for (size_t i = 0; i < hex.length(); i += 2) {
+        std::string byteString = hex.substr(i, 2);
+        char byte = static_cast<char>(strtol(byteString.c_str(), nullptr, 16));
+        binaryData.push_back(byte);
+    }
+    return binaryData;
+}
 
-int main(int argc, char* argv[]) {
+int main(int argc, char *argv[]) {
     if (argc != 3) {
-        std::cerr << "Usage: " << argv[0] << " <server_ip> <port>" << std::endl;
-        return EXIT_FAILURE;
+        std::cerr << "Usage: " << argv[0] << " <server IP> <port>" << std::endl;
+        return 1;
     }
 
-    const char* server_ip = argv[1];
-    int port = std::stoi(argv[2]);
-    if (port <= 0) {
-        std::cerr << "Invalid port number." << std::endl;
-        return EXIT_FAILURE;
+    std::string serverIP = argv[1];
+    int port = atoi(argv[2]);
+
+    // Create socket
+    int sock = socket(AF_INET, SOCK_STREAM, 0);
+    if (sock < 0) {
+        std::cerr << "Socket creation failed." << std::endl;
+        return 1;
     }
 
-    int sock = 0;
-    sockaddr_in serv_addr;
-    char buffer[BUFFER_SIZE] = {0};
-
-    // Create socket file descriptor
-    if ((sock = socket(AF_INET, SOCK_STREAM, 0)) < 0) {
-        std::cerr << "Socket creation error" << std::endl;
-        return EXIT_FAILURE;
+    // Fill in server address information
+    sockaddr_in serverAddr{};
+    serverAddr.sin_family = AF_INET;
+    serverAddr.sin_port = htons(port);
+    if (inet_pton(AF_INET, serverIP.c_str(), &serverAddr.sin_addr) <= 0) {
+        std::cerr << "Invalid address / Address not supported." << std::endl;
+        close(sock);
+        return 1;
     }
 
-    serv_addr.sin_family = AF_INET;
-    serv_addr.sin_port = htons(port);
-
-    // Convert IPv4 and IPv6 addresses from text to binary form
-    if (inet_pton(AF_INET, server_ip, &serv_addr.sin_addr) <= 0) {
-        std::cerr << "Invalid address/ Address not supported" << std::endl;
-        return EXIT_FAILURE;
+    // Connect to server
+    if (connect(sock, reinterpret_cast<sockaddr*>(&serverAddr), sizeof(serverAddr)) < 0) {
+        std::cerr << "Connection Failed." << std::endl;
+        close(sock);
+        return 1;
     }
 
-    // Connect to the server
-    if (connect(sock, reinterpret_cast<sockaddr*>(&serv_addr), sizeof(serv_addr)) < 0) {
-        std::cerr << "Connection Failed" << std::endl;
-        return EXIT_FAILURE;
-    }
-
-    std::string message;
-    while (true) {
-        std::cout << "Enter message to send to server: ";
-        std::getline(std::cin, message);
-
-        // Send the message to the server
-        send(sock, message.c_str(), message.size(), 0);
-
-        // Read the echoed message from the server
-        memset(buffer, 0, BUFFER_SIZE);
-        int bytes_read = read(sock, buffer, BUFFER_SIZE);
-        if (bytes_read <= 0) {
-            std::cerr << "Read failed or connection closed by server" << std::endl;
-            break;
-        }
-        std::cout << "Message from server: " << buffer << std::endl;
+    std::string inputLine;
+    while (std::getline(std::cin, inputLine)) {
+        auto binaryData = hexToBinary(inputLine);
+        send(sock, binaryData.data(), binaryData.size(), 0);
     }
 
     close(sock);
